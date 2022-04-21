3d5a0858447812de8a6c9daa865d3c50039005f1
valeriangrip <92164242+Valeriangrip@users.noreply.github.com>
2022-04-21 19:19:09 -0400

    complete both mintable and crowdsale contracts

--- KaseiCoinCrowdsale.sol
+++ KaseiCoinCrowdsale.sol
@@ -8,11 +8,13 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5
 // Have the KaseiCoinCrowdsale contract inherit the following OpenZeppelin:
 // * Crowdsale
 // * MintedCrowdsale
-contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
+contract KaseiCoinCrowdsale is Crowdsale, MintedCrowdsale{ // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANCE
     
     // Provide parameters for all of the features of your crowdsale, such as the `rate`, `wallet` for fundraising, and `token`.
     constructor(
-        // YOUR CODE HERE!
+        uint rate,
+        address payable wallet,
+        KaseiCoin token
     ) public Crowdsale(rate, wallet, token) {
         // constructor can stay empty
     }
@@ -21,17 +23,30 @@ contract KaseiCoinCrowdsale { // UPDATE THE CONTRACT SIGNATURE TO ADD INHERITANC
 /*
 contract KaseiCoinCrowdsaleDeployer {
     // Create an `address public` variable called `kasei_token_address`.
-    // YOUR CODE HERE!
+    address public kasei_token_address;
+    address public kasei_crowdsale_address;
+
     // Create an `address public` variable called `kasei_crowdsale_address`.
     // YOUR CODE HERE!
 
     // Add the constructor.
     constructor(
        // YOUR CODE HERE!
+        string memory name,
+        string memory symbol,
+        address payable wallet
+
     ) public {
         // Create a new instance of the KaseiCoin contract.
         // YOUR CODE HERE!
-        
+        KaseiCoin token = new KaseiCoin(name, symbol, 0);
+        kasei_token_address = address(token);
+
+        KaseiCoinCrowdsale kasei_crowdsale = new KaseiCrowdsale(1, wallet, token);
+        kasei_crowdsale_address = address(kasei_crowdsale);
+
+        token.addMinter(kasei_crowdsale_address);
+        token.renounceMinter();
         // Assign the token contract’s address to the `kasei_token_address` variable.
         // YOUR CODE HERE!
 
